c93073d0e6b0ba8772201d64819aa86ca9c1c9d6
junghyunsoo24 <sunni303356@naver.com>
2023-11-06 06:33:58 +0900

    청소년 모니 터링 전문가 기능  주황색 계열로 색상 변경

--- app/src/main/java/com/example/portfolioteenageremotionpreventappexpertandmanager/AllMainActivity.kt
+++ app/src/main/java/com/example/portfolioteenageremotionpreventappexpertandmanager/AllMainActivity.kt
@@ -2,6 +2,8 @@ package com.example.portfolioteenageremotionpreventappexpertandmanager
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.portfolioteenageremotionpreventappexpertandmanager.appViewModel.AppViewModel
 import com.example.portfolioteenageremotionpreventappexpertandmanager.databinding.ActivityAllMainBinding
@@ -13,6 +15,16 @@ class AllMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         viewModel = AppViewModel.getInstance()
+
+        val actionBar: ActionBar? = supportActionBar
+
+        actionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        actionBar?.setCustomView(R.layout.actionbar_all)
+
+        val actionBarTitle = actionBar?.customView?.findViewById<TextView>(R.id.actionBarAll)
+        actionBarTitle?.text = "하루친구(전문가,관리자)"
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
         val binding = ActivityAllMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
